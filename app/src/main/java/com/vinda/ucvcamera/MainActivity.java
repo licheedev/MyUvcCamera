ef60e32a22f1fdb6e57ca267113a8dc7e0ca27de
licheedev <licheedev@foxmail.com>
2021-05-22 15:02:08 +0800

    修改依赖，使用AndroidX

--- app/src/main/java/com/vinda/ucvcamera/MainActivity.java
+++ app/src/main/java/com/vinda/ucvcamera/MainActivity.java
@@ -3,7 +3,7 @@ package com.vinda.ucvcamera;
 import android.graphics.SurfaceTexture;
 import android.hardware.usb.UsbDevice;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.Log;
 import android.view.Surface;
 import android.view.View;
